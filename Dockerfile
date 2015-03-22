2ffd442027aa757b1cded003ecc868094088d245
Jack Davis <mr.jackdavis@gmail.com>
2015-03-22 18:59:11 +1100

    Add test endpoint that retrieves data from the api

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ RUN apt-get update
 
 RUN pip install cairocffi
 RUN pip install Flask
+RUN pip install nap
 
 ADD ./src/* /app/
 
--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 from flask import Flask,send_file
+from nap.url import Url
+import urllib.request
+import os
 import generator
 import logging
 
@@ -7,10 +10,32 @@ app.debug = True
 
 logging.basicConfig(level=logging.DEBUG)
 
-@app.route('/')
+@app.route('/example')
 def fractal_example():
+
 	fileLocation = generator.generateExample()
 	return send_file(fileLocation, mimetype='image/gif')
+
+@app.route('/testapi')
+def api_test():
+	apiLocation = os.environ['API_PORT']
+
+	apiLocation = apiLocation.replace('tcp://','http://') + '/'
+
+	logging.debug(apiLocation)
+
+	api = Url(apiLocation) #API_PORT from docker
+
+	# Get scenes
+	scenes = api.get('scene').json()
+
+	logging.debug(scenes)
+
+	resourceURL = scenes[0]['resource']['location']
+	response = urllib.request.urlopen(resourceURL)
+	data = response.read()
+	text = data.decode('utf-8')
+	return text
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')

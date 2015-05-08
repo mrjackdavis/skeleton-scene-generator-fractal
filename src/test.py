b590c7b6a837b9f3cc58e99bec77316d882494ae
Jack Davis <mr.jackdavis@gmail.com>
2015-05-08 12:00:01 +1000

    Fix issue with test.py

--- src/test.py
+++ src/test.py
@@ -5,18 +5,20 @@ import logging
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 from boto.s3.connection import Location
-from SklItemApi import SklItemApi
+from SklItem import SklItem
+from SklProcess import SklProcess
 
 logging.basicConfig(level=logging.DEBUG)
 
 
 
-logging.info('Running process',len(items))
+logging.info('Running test')
 
 item = SklItem('test',time.time(),'http://www.google.com')
+process = SklProcess('1',item,"InProgress")
 
 fileLocation = generator.new(process)
-logging.info("Finished generating %s:%s",item.id,process.id)
+logging.info("Finished generating %s",item.id)
 
 if not fileLocation:
 	raise Exception("File location was null")
